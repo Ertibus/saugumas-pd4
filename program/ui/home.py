3819c73f17ccc6a622bc20be9f980726acfa5f43
ertibus <ertibus@localhost>
2021-05-22 16:20:43 +0300

    UI_fin Changes to be committed: modified:   program/ui/home.py

--- program/ui/home.py
+++ program/ui/home.py
@@ -2,8 +2,68 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
+from PyQt5.Qt import QRect
 
 from program.utils import Utilities
+
+class Popup(QWidget):
+    def __init__(self, btn_name:str, btn_func):
+        QWidget.__init__(self)
+        self.btn_name = btn_name
+        self.btn_func = btn_func
+        self.setWindowTitle(self.btn_name + " password")
+
+        self.initUI()
+    
+    def initUI(self):
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        layout.setRowStretch(1, 1)
+        layout.setRowStretch(99, 1)
+
+        layout.setColumnStretch(1, 3)
+        layout.setColumnStretch(3, 2)
+
+        login_lbl = QLabel("Login name:")
+        login_lbl.setAlignment(Qt.AlignRight)
+        layout.addWidget(login_lbl, 2, 1)
+
+        login_inp = QLineEdit()
+        login_inp.setPlaceholderText("example@login.io")
+        layout.addWidget(login_inp, 2, 2)
+
+        pass_lbl = QLabel("Password:")
+        pass_lbl.setAlignment(Qt.AlignRight)
+        layout.addWidget(pass_lbl, 3, 1)
+
+        pass_inp = QLineEdit()
+        pass_inp.setEchoMode(QLineEdit.Password)
+        pass_inp.setPlaceholderText("password123")
+        layout.addWidget(pass_inp, 3, 2)
+
+        url_lbl = QLabel("URL/Application:")
+        url_lbl.setAlignment(Qt.AlignRight)
+        layout.addWidget(url_lbl, 4, 1)
+
+        url_inp = QLineEdit()
+        url_inp.setPlaceholderText("www.facebook.com")
+        layout.addWidget(url_inp, 4, 2)
+
+        desc_lbl = QLabel("Description:")
+        desc_lbl.setAlignment(Qt.AlignRight)
+        layout.addWidget(desc_lbl, 5, 1)
+
+        desc_inp = QLineEdit()
+        desc_inp.setPlaceholderText("Social Media")
+        layout.addWidget(desc_inp, 5, 2)
+
+        sep_lbl = QLabel(" ")
+        layout.addWidget(sep_lbl, 6, 1)
+
+        create_btn = QPushButton(self.btn_name)
+        create_btn.clicked.connect(self.btn_func)
+        layout.addWidget(create_btn, 7, 1, 1, 3)
 
 class Home():
     def __init__(self, root, listener, uid):
@@ -15,73 +75,181 @@ class Home():
     def new_password(self):
         def _create():
             pass
-        self.screen_lay.setRowStretch(1, 1)
-        self.screen_lay.setRowStretch(99, 1)
 
-        self.screen_lay.setColumnStretch(1, 3)
-        self.screen_lay.setColumnStretch(3, 2)
+        self.popup = Popup("Create", _create)
+        self.popup.show()
 
-        title_lbl = QLabel("Create new password")
-        title_lbl.setFont(QFont('monospace', 24))
-        title_lbl.setAlignment(Qt.AlignCenter)
-        self.screen_lay.addWidget(title_lbl, 1, 1, 1, 3)
+    def upd_password(self):
+        def _update():
+            pass
+        self.popup = Popup("Update", _update)
+        self.popup.show()
+
+    def find_password(self):
+        def _search():
+            pass
+
+        def _next():
+            self.entry_id += 1
+            if len(self.entry_arr) < self.entry_id - 1:
+                self.entry_id = len(self.entry_arr) - 1
+            _draw_list()
+
+        def _prev():
+            self.entry_id -= 1
+            if self.entry_id < 0:
+                self.entry_id = 0
+            _draw_list()
+
+        def _draw_list():
+            Utilities.clear_layout(entry_layout)
+
+            login_lbl = QLabel("Login name:")
+            login_lbl.setAlignment(Qt.AlignCenter)
+            entry_layout.addWidget(login_lbl, 0, 0)
+
+            pass_lbl = QLabel("Password:")
+            pass_lbl.setAlignment(Qt.AlignCenter)
+            entry_layout.addWidget(pass_lbl, 0, 1)
+
+            url_lbl = QLabel("URL/Application:")
+            url_lbl.setAlignment(Qt.AlignCenter)
+            entry_layout.addWidget(url_lbl, 0, 4)
+
+            desc_lbl = QLabel("Description:")
+            desc_lbl.setAlignment(Qt.AlignCenter)
+            entry_layout.addWidget(desc_lbl, 0, 5)
+
+            for i in range(0, 10):
+                if i + self.entry_id >= len(self.entry_arr):
+                    break;
+                login_inp = QLineEdit()
+                login_inp.setReadOnly(True)
+                login_inp.setPlaceholderText("example@login.io")
+                entry_layout.addWidget(login_inp, i, 0)
+
+                pass_inp = QLineEdit()
+                pass_inp.setReadOnly(True)
+                pass_inp.setPlaceholderText("password123")
+                entry_layout.addWidget(pass_inp, i, 1)
+
+                show_btn = QPushButton("*")
+                show_btn.clicked.connect(_search)
+                entry_layout.addWidget(show_btn, i, 2)
+
+                copy_btn = QPushButton("Copy")
+                copy_btn.clicked.connect(_search)
+                entry_layout.addWidget(copy_btn, i, 3)
+
+                url_inp = QLineEdit()
+                url_inp.setReadOnly(True)
+                url_inp.setPlaceholderText("www.facebook.com")
+                entry_layout.addWidget(url_inp, i, 4)
+
+                desc_inp = QLineEdit()
+                desc_inp.setReadOnly(True)
+                desc_inp.setPlaceholderText("Social Media")
+                entry_layout.addWidget(desc_inp, i, 5)
+
+        self.entry_id = 0
+        self.entry_arr = []
+
+        layout = QGridLayout()
+        self.screen_lay.addLayout(layout)
+
+        layout.setRowStretch(1, 1)
+        layout.setRowStretch(99, 1)
+
+        layout.setColumnStretch(2, 1)
+
+
+        sep_lbl = QLabel(" ")
+        layout.addWidget(sep_lbl, 1, 1)
 
         login_lbl = QLabel("Login name:")
         login_lbl.setAlignment(Qt.AlignRight)
-        self.screen_lay.addWidget(login_lbl, 2, 1)
+        layout.addWidget(login_lbl, 2, 1)
 
         login_inp = QLineEdit()
         login_inp.setPlaceholderText("example@login.io")
-        self.screen_lay.addWidget(login_inp, 2, 2)
+        layout.addWidget(login_inp, 2, 2,)
+
+        create_btn = QPushButton("Search")
+        create_btn.clicked.connect(_search)
+        layout.addWidget(create_btn, 2, 3,)
+
+        sep_lbl = QLabel(" ")
+        layout.addWidget(sep_lbl, 3, 1)
+
+
+        entry_layout = QGridLayout()
+        entry_layout.setColumnStretch(5, 1)
+
+        entry_group = QGroupBox("Password list:")
+        entry_group.setLayout(entry_layout)
+        layout.addWidget(entry_group, 4, 1, 1, 4)
+
+        _draw_list()
+
+        page_btn_lay = QHBoxLayout()
+        layout.addLayout(page_btn_lay, 5, 1, 1, 3)
+
+        page_btn_lay.addStretch()
+
+        prev_btn = QPushButton("Prev")
+        prev_btn.clicked.connect(_prev)
+        page_btn_lay.addWidget(prev_btn)
+
+        next_btn = QPushButton("Next")
+        next_btn.clicked.connect(_next)
+        page_btn_lay.addWidget(next_btn)
+
+
+    def del_password(self):
+        def _delete():
+            pass
+        layout = QGridLayout()
+
+        layout.setRowStretch(1, 1)
+        layout.setRowStretch(99, 1)
+
+        layout.setColumnStretch(1, 3)
+        layout.setColumnStretch(3, 2)
+
+        login_lbl = QLabel("Login name:")
+        login_lbl.setAlignment(Qt.AlignRight)
+        layout.addWidget(login_lbl, 2, 1)
+
+        login_inp = QLineEdit()
+        login_inp.setPlaceholderText("example@login.io")
+        layout.addWidget(login_inp, 2, 2)
 
         pass_lbl = QLabel("Password:")
         pass_lbl.setAlignment(Qt.AlignRight)
-        self.screen_lay.addWidget(pass_lbl, 3, 1)
+        layout.addWidget(pass_lbl, 3, 1)
 
         pass_inp = QLineEdit()
         pass_inp.setEchoMode(QLineEdit.Password)
         pass_inp.setPlaceholderText("password123")
-        self.screen_lay.addWidget(pass_inp, 3, 2)
-
-        url_lbl = QLabel("URL/Application:")
-        url_lbl.setAlignment(Qt.AlignRight)
-        self.screen_lay.addWidget(url_lbl, 4, 1)
-
-        url_inp = QLineEdit()
-        url_inp.setPlaceholderText("www.facebook.com")
-        self.screen_lay.addWidget(url_inp, 4, 2)
-
-        desc_lbl = QLabel("Description:")
-        desc_lbl.setAlignment(Qt.AlignRight)
-        self.screen_lay.addWidget(desc_lbl, 5, 1)
-
-        desc_inp = QLineEdit()
-        desc_inp.setPlaceholderText("Social Media")
-        self.screen_lay.addWidget(desc_inp, 5, 2)
+        layout.addWidget(pass_inp, 3, 2)
 
         sep_lbl = QLabel(" ")
-        self.screen_lay.addWidget(sep_lbl, 6, 1)
+        layout.addWidget(sep_lbl, 6, 1)
 
-        create_btn = QPushButton("Create")
-        create_btn.clicked.connect(_create)
-        self.screen_lay.addWidget(create_btn, 7, 1, 1, 3)
+        create_btn = QPushButton("Delete")
+        create_btn.clicked.connect(_delete)
+        layout.addWidget(create_btn, 7, 1, 1, 3)
 
-    def upd_password(self):
-        self.screen_lay.setRowStretch(99, 1)
-
-    def find_password(self):
-        self.screen_lay.setRowStretch(99, 1)
-
-    def del_password(self):
-        self.screen_lay.setRowStretch(99, 1)
+        self.window = QWidget()
+        self.window.setWindowTitle("Delete Password")
+        self.window.setLayout(layout)
+        self.window.show()
 
     def initUI(self):
         def _new():
-            Utilities.clear_layout(self.screen_lay)
             self.new_password()
 
         def _upd():
-            Utilities.clear_layout(self.screen_lay)
             self.upd_password()
 
         def _find():
@@ -89,7 +257,6 @@ class Home():
             self.find_password()
 
         def _del():
-            Utilities.clear_layout(self.screen_lay)
             self.del_password()
 
         def _off():
@@ -99,9 +266,8 @@ class Home():
         main_layout = QVBoxLayout()
         self.root.addLayout(main_layout)
 
-        # +---------+
         # | Buttons |
-        # +---------+
+
         btn_lay = QHBoxLayout()
         main_layout.addLayout(btn_lay)
 
@@ -128,6 +294,7 @@ class Home():
         off_btn.clicked.connect(_off)
         btn_lay.addWidget(off_btn)
 
-        self.screen_lay = QGridLayout()
-        self.screen_lay.setRowStretch(99, 1)
+        self.screen_lay = QVBoxLayout()
         main_layout.addLayout(self.screen_lay)
+
+        _find()
